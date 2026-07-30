58737937cdb7a8fc1ac2660350db024e6b35429c
Ichibannnn <guser9897@gmail.com>
2026-07-30 16:54:06 +0800

    Setting up shadcn Toggle Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,62 @@
-import Image from "next/image";
+import { ThemeToggle } from "@/components/theme-toggle";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Button, Input } from "@base-ui/react";
 
 export default function Home() {
-  return <h1>Wallet Watch</h1>;
+  return (
+    <main className="flex min-h-screen flex-col gap-4 p-8">
+      <div className="flex w-full gap-4">
+        <h1 className="text-2xl font-semibold">Wallet Watch</h1>
+        <ThemeToggle />
+      </div>
+
+      <div>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>Enter your email below to login to your account</CardDescription>
+            <CardAction>
+              <Button>Sign Up</Button>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input id="email" type="email" placeholder="m@example.com" required />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a href="#" className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button className="w-full">Login with Google</Button>
+          </CardFooter>
+        </Card>
+      </div>
+
+      {/* rest of your page content */}
+    </main>
+  );
 }
